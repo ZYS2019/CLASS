89fac0bc07d3383659f681e92a7b1971d116e476
ZYS <ZYS@DEMPF3B12WV9B6Q>
2020-02-21 15:24:53 +0800

    添加第二节课项目

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -1,9 +1,34 @@
-#include<opencv.hpp>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
 
-using namespace cv;
+#define totalNum 100
 
-int main()
-{
-	Mat src = imread("C:\\Users\\Administrator\\Desktop\\≤‚ ‘”√Õº\\222.jpg");
+int isPrime(int n);
+
+int main(void) {
+	int i;
+	using namespace std;
+	for (i = 1; i < totalNum; i++) {
+		if (isPrime(i)) {
+			cout << i << " ";
+		}
+	}
+	system("pause");
 	return 0;
+}
+
+int isPrime(int n) {
+	int i;
+	int flag = 1;
+	if (n == 1 || n % 2 == 0) flag = 0;
+	for (i = 3; i <= sqrt(n); i += 2) {
+		if (n % i == 0) {
+			flag = 0;
+			break;
+		}
+	}
+	if (n == 2) flag = 1;
+	return flag;
 }
